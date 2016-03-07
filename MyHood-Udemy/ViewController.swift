4708378fe5c174d5043d69656fef6724e85f0029
Emmanuel Darmon <emmanueldarmon@mbp-de-emmanuel.home>
2016-03-07 20:29:17 +0100

    design layou done

--- MyHood-Udemy/ViewController.swift
+++ MyHood-Udemy/ViewController.swift
@@ -8,18 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate {
+    
+    @IBOutlet weak var tableview: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 

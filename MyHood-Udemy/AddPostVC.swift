bff7b0366d4cce129eb3ecc82dc4d9d1bf096d0f
Emmanuel Darmon <emmanueldarmon@mbp-de-emmanuel.home>
2016-03-08 14:51:33 +0100

    new view contoller

---
+++ MyHood-Udemy/AddPostVC.swift
@@ -0,0 +1,15 @@
+//
+//  AddPostVC.swift
+//  MyHood-Udemy
+//
+//  Created by Emmanuel Darmon on 08/03/2016.
+//  Copyright © 2016 Emmanuel Darmon. All rights reserved.
+//
+
+import UIKit
+
+class AddPostVC: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
